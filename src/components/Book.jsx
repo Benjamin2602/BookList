0840c2da1bdcf456f0c893c8225a7c35c4b5ba6d
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-01 12:14:37 +0530

    refactoring done in booklist (map)

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// const title =  'Atomic Habits'
-// // const author = 'james clear'
-// const img = 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg'
-
 const Book = ({author,title,img}) => {
 
   return (
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,41 +2,54 @@ import React from 'react'
 import Book from './Book'
 
 
+const books = [
+  {
+    id: 1,
+    author: "James Clear",
+    title: 'Atomic Habits',
+    img: 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 2,
+    author: "Nick Trenton",
+    title: 'Stop Overthinking',
+    img: 'https://m.media-amazon.com/images/I/41STeQ+eLQL._SX311_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 3,
+    author: "Ramit Sethi",
+    title: 'I will teach you to be rich',
+    img: 'https://m.media-amazon.com/images/I/51eDMGwZVvL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+  },
+  {
+    id: 4,
+    author: " Ann Napolitano",
+    title: 'Hello Beautiful',
+    img: 'https://m.media-amazon.com/images/I/51k+9K+aFCL._SY344_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 5,
+    author: "MJ DeMarco",
+    title: 'The Millionaire Fastlane',
+    img: 'https://m.media-amazon.com/images/I/41lFb7+HGKL.jpg'
+  },
+  {
+    id: 6,
+    author: "collen hoover",
+    title: 'Ugly Love',
+    img: 'https://m.media-amazon.com/images/I/41mBCtaJ1XL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
+  },
+
+]
+
+
 const BookList = () => {
   return (
     <section className="book-list">
-    <Book
-     author= "James Clear"
-      title={ 'Atomic Habits'}
-      img= 'https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg'
-     />
-    <Book
-      author= "Nick Trenton"
-      title={ 'Stop Overthinking'}
-      img= 'https://m.media-amazon.com/images/I/41STeQ+eLQL._SX311_BO1,204,203,200_.jpg'
-    />
-    <Book
-      author= "Ramit Sethi"
-      title={ 'I will teach you to be rich'}
-      img = 'https://m.media-amazon.com/images/I/51eDMGwZVvL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-    />
-    <Book
-      author= " Ann Napolitano"
-      title={ 'Hello Beautiful'}
-      img= 'https://m.media-amazon.com/images/I/51k+9K+aFCL._SY344_BO1,204,203,200_.jpg'
-    />
-
-    <Book
-      author= "MJ DeMarco"
-      title={ 'The Millionaire Fastlane'}
-      img= 'https://m.media-amazon.com/images/I/41lFb7+HGKL.jpg'
-    />
-
-    <Book
-      author= "collen hoover"
-      title={ 'Ugly Love'}
-      img = 'https://m.media-amazon.com/images/I/41mBCtaJ1XL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-    />
+      {books.map((book) => {
+        const {id,author,title,img} = book
+        return <Book key={id} img = {img} title={title} author={author}/>
+      })}
       
     </section>
   )
